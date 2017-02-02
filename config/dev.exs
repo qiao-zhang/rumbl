aa7c46b3ddde09f26ddc88ab272f35bd78184775
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-01 20:34:54 -0500

    reimplement Repo with database

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "yspcq",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
