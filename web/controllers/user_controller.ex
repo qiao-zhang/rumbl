6bbaa7404cb2a38fd00c8e43e6e4fb462387cf63
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-02 09:08:22 -0500

    show form for adding user

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -6,9 +6,14 @@ defmodule Rumbl.UserController do
     users = Repo.all(User)
     render conn, "index.html", users: users
   end
-  
+
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
